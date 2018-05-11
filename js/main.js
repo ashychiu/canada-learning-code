303f15a1f0577b8e1a9c66baafcc1340ce0d59cc
ashychiu <32230130+ashychiu@users.noreply.github.com>
2018-05-10 20:56:39 -0700

    Files added

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$('.home').click(function(){
+ $('nav').css('background-image','url("images/background0.jpg")');
+ $('footer').css('background-color','#38b6ab');
+ $(".main-nav").removeClass('open-nav');
+});
+ $('.about').click(function(){
+ $('nav').css('background-image','url("images/background2.jpg")');
+ $('footer').css('background-color','#7e85b3');
+ $(".main-nav").removeClass('open-nav');
+});
+$('.collections').click(function(){
+ $('nav').css('background-image','url("images/background3.jpg")');
+ $('footer').css('background-color','#9f687f');
+ $(".main-nav").removeClass('open-nav');
+ });
+$('.location').click(function(){
+ $('nav').css('background-image','url("images/background4.jpg")');
+ $('footer').css('background-color','#b93d3d');
+ $(".main-nav").removeClass('open-nav');
+});
+$('.contact').click(function(){
+ $('nav').css('background-image','url("images/background5.jpg")');
+ $('footer').css('background-color','#bf7912');
+ $(".main-nav").removeClass('open-nav');
+});
+$(".hamburger").click(function(e){
+  e.preventDefault();
+  $(".main-nav").addClass('open-nav');
+});
+$(".close").click(function(e){
+  e.preventDefault();
+  $(".main-nav").removeClass('open-nav');
+});
+$("nav a").click(function(){
+  $("nav a").removeClass("active");
+  $(this).addClass('active');
+});
